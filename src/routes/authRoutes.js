f098ff380fc3f34b03f229856cb7757b2e65d299
Michael Fraser <michaelfraser2001@gmail.com>
2025-01-28 18:10:34 +0000

    Fixed bug.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { register, login, logout } from '../controllers/authController.js';
+import { register, login } from '../controllers/authController.js';
 
 const router = Router();
 
